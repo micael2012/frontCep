10b7dbd21b02479fd45e3a2725bca263923d98c5
Micael Itaira Costa Pereira <AB1306362@br.atento.com>
2020-03-09 01:38:19 -0300

    primeiro

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/app.css">       
+    </head>
+    <body>
+    
+    </body>
+</html>
